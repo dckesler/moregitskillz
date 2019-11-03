99b1dbdfd7095de1a22c746765ef8519175b1e7b
Jim Schlader <jamesschlader@yahoo.com>
2019-11-03 15:49:49 -0700

    printing out the hour

--- jims_awesome_file.js
+++ jims_awesome_file.js
@@ -1,11 +1,19 @@
 const today = new Date();
+const currentHour = today.getHours();
 
 const printIt = () => {
   console.log("Jim's awesome file!");
-  if (today.getHours() < 12) {
+
+  if (currentHour < 12) {
     console.log("It's AM!");
+    console.log(
+      `It's ${currentHour} AM. Only ${12 - currentHour} hours to go til noon!`
+    );
   } else {
     console.log("It's PM!");
+    console.log(
+      `It's ${currentHour} PM and only ${24 - currentHour} hours til midnight!`
+    );
   }
   console.log(today);
 };
