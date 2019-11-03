6c8a986026e516d4649335283400a0331b8c86de
Jim Schlader <jamesschlader@yahoo.com>
2019-11-03 15:19:58 -0700

    Added some real functionality

--- jims_awesome_file.js
+++ jims_awesome_file.js
@@ -1,1 +1,13 @@
-module.exports = console.log("Jim's repaired super awesome file!");
+const today = new Date();
+
+const printIt = () => {
+  console.log("Jim's awesome file!");
+  if (today.getHours() < 12) {
+    console.log("It's AM!");
+  } else {
+    console.log("It's PM!");
+  }
+  console.log(today);
+};
+
+module.exports = printIt;
